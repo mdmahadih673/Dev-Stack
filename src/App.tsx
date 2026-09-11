7011a08c0d1ea31336f2de7f265abb13bfb10465
mdmahadih673 <mdmahadih673@gmail.com>
2026-09-12 00:22:53 +0600

    Technology folder and StackItem, Technologies, TechnologyCard, YourStack add kora hoiche and card section is ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import { Suspense, useState } from "react"
 import Hero from "./components/Hero/Hero"
 import Navbar from "./components/Navbar/Navbar"
+import Technologies from "./components/Technology/Technologies"
 import type { ITechnologyType } from "./types/TechnologyType"
+import { ToastContainer } from "react-toastify"
+import Footer from "./components/Footer/Footer"
 
 const technologiesfetch = async (): Promise<ITechnologyType[]> => {
   const res = await fetch('/technologies.json')
@@ -8,16 +12,20 @@ const technologiesfetch = async (): Promise<ITechnologyType[]> => {
   return data
 }
 function App() {
+  const [technologiesPromise] = useState(() => technologiesfetch())
 
 
   return (
     <>
       <div className="container mx-auto">
-
+        <ToastContainer />
         <Navbar />
         <Hero />
-
+        <Suspense fallback={<h1>Loading.........</h1>}>
+          <Technologies technologiesPromise={technologiesPromise} />
+        </Suspense>
       </div>
+      <Footer />
 
     </>
   )
